19c2df0c6e5bdd412afe62c91eeb57449f681344
Murilo <murilodesousa454@gmail.com>
2026-07-12 01:41:46 -0300

    Create Flor.h

---
+++ Arquivos/Flor.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Produto.h"
+
+class Flor : public Produto {
+private:
+  string cor, formato;
+
+public:
+  Flor(string n, float p, int i, int q, string co, string f) : Produto(n, p, i, q), cor(co), formato(f) {}
+
+  void exibir() {
+    Produto::exibir();
+    cout << "Cor: " << cor << endl << "Formato: " << formato << endl;
+  }
+
+  string getTipo() { return "F"; }
+
+  void salvar(ofstream &out) {
+    out << "F-";
+    Produto::salvar(out);
+    out << cor << "-" << formato << endl;
+  }
+};
