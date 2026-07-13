449290eb53d5189d13c215989cb7184268bd27c6
Murilo <murilodesousa454@gmail.com>
2026-07-13 15:16:25 -0300

    Update Data.h

--- Arquivos/Data.h
+++ Arquivos/Data.h
@@ -7,9 +7,6 @@ protected:
 
 public:
     Data(int d, int m, int a) : dia(d), mes(m), ano(a) {}
-    int getDia() { return dia; }
-    int getMes() { return mes; }
-    int getAno() { return ano; }
     void exibir() {
         cout << "Data de Cadastro: " << dia << "/" << mes << "/" << ano << endl;
     }
