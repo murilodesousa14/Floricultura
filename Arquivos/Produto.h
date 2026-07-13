95fef1effc1aa6baaf531a54ca09dec1d8729223
Murilo <murilodesousa454@gmail.com>
2026-07-13 15:14:06 -0300

    Update Produto.h

--- Arquivos/Produto.h
+++ Arquivos/Produto.h
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include "Data.h"
 
 using namespace std;
 
-class Produto {
+class Produto : public Data {
 protected:
   string nome;
   float preco;
@@ -13,18 +14,19 @@ protected:
   int quantidade;
 
 public:
-  Produto(string n, float p, int i, int q) : nome(n), preco(p), indice(i), quantidade(q) {}
+  Produto(int d, int m, int a, string n, float p, int i, int q) : Data(d, m, a), nome(n), preco(p), indice(i), quantidade(q) {}
   virtual ~Produto() {}
 
   virtual string getTipo() = 0;
   virtual void salvar(ofstream &out) {
-    out << nome << "-" << preco << "-" << indice << "-" << quantidade << "-";
+    out << dia << "-" << mes << "-" << ano << "-" << nome << "-" << preco << "-" << indice << "-" << quantidade << "-";
   }
   virtual void exibir() {
     cout << "\nNome: " << nome << endl
          << "Preço: R$ " << preco << endl
          << "Código: " << indice << endl
          << "Quantidade em Estoque: " << quantidade << endl;
+         Data::exibir();
   }
 
   string getNome() { return nome; }
